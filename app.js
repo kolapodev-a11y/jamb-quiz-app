d7e641711575ab83fdce444c08a1f082f86eb898
kolapodev-a11y <kolapodev@gmail.com>
2026-02-10 21:48:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -448,4 +448,11 @@ if (document.readyState === 'loading') {
 } else {
     initApp();
 }
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
 
